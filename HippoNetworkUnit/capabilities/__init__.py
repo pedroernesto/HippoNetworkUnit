c0768d1f3834bf19d594d04194bfb284da305f2a
Pedro Garcia <pedro.garcia@unic.cnrs-gif.fr>
2017-11-09 17:21:53 +0100

    Updating a draft test for synapses density across CA1 layers

--- HippoNetworkUnit/capabilities/__init__.py
+++ HippoNetworkUnit/capabilities/__init__.py
@@ -1,3 +1,5 @@
 # Loads Python classes for capabilities needed in HippoNetworkUnit
 
 # capabilities/
+from . import cap_ProvidesCA1NeuritePathDistanceInfo
+from . import cap_Provides_CA1_laminar_distribution_synapses_info
--- HippoNetworkUnit/tests/__init__.py
+++ HippoNetworkUnit/tests/__init__.py
@@ -1,4 +1,6 @@
 # Loads Python classes for validation-tests to be used in HippoNetworkUnit
 
 # tests/
+from . import test_CA1_laminar_distribution_synapses
+from . import test_CA1Layers_NeuritePathDistance_MeanSD
 
