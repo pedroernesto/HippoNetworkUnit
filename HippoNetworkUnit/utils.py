8b947a8e6ced45101c6a2c1ad33cbc04db3c195e
Pedro Garcia <pedro.garcia@unic.cnrs-gif.fr>
2018-06-11 16:16:06 +0200

    Minor fix

--- HippoNetworkUnit/utils.py
+++ HippoNetworkUnit/utils.py
@@ -22,28 +22,6 @@ class CA1_laminar_distribution_synapses(sciunit.Model):
     def get_CA1_laminar_distribution_synapses_info(self):
         return self.CA1_laminar_distribution_synapses_info
 
-    class CA1Layers_NeuritePathDistance(sciunit.Model):
-        instance_id = "bb06ab0a-685c-4f0f-b078-195cd947639f"
-
-        # model_instance_uuid = # prod
-
-        def __init__(self, name='CA1Layers_NeuritePathDistance', CA1LayersNeuritePathDistance_info={}):
-            self.CA1LayersNeuritePathDistance_info = CA1LayersNeuritePathDistance_info
-            sciunit.Model.__init__(self, name=name)
-            self.name = name
-            self.description = "Dummy model to test neurite path-distances across CA1 layers"
-            self.set_CA1LayersNeuritePathDistance_info_default()
-
-        def set_CA1LayersNeuritePathDistance_info_default(self):
-            self.CA1LayersNeuritePathDistance_info = {"SLM": {'PathDistance': {'value': '120 um'}},
-                                                      "SR": {'PathDistance': {'value': '280 um'}},
-                                                      "SP": {'PathDistance': {'value': '40 um'}},
-                                                      "SO": {'PathDistance': {'value': '100 um'}}
-                                                      }
-
-        def get_CA1LayersNeuritePathDistance_info(self):
-            return self.CA1LayersNeuritePathDistance_info
-
 
 # ==============================================================================
 
